66cf1c62a1ddd364a897285ac252e9d596f02c63
Amir55143 <aamirnoor55143@gmail.com>
2023-09-19 14:46:37 +0500

    Routes

--- lib/screen/screen_1.dart
+++ lib/screen/screen_1.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent[100],
+      backgroundColor: Colors.teal[100],
       appBar: AppBar(
-        title: const Text('Routes'),
+        title: const Text('Screen 1'),
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pushNamed(context, 'screen1');
+            Navigator.pushNamed(context, 'screen2');
           }, 
           child: const Text('Navigator'),
           ),
--- lib/screen/screen_2.dart
+++ lib/screen/screen_2.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class Screen2 extends StatelessWidget {
+  const Screen2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent[100],
+      backgroundColor: Colors.teal[200],
       appBar: AppBar(
-        title: const Text('Routes'),
+        title: const Text('Screen 2'),
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pushNamed(context, 'screen1');
+            Navigator.pushNamed(context, 'screen2');
           }, 
           child: const Text('Navigator'),
           ),
